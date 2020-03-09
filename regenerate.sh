3f6d391cc22b11ef5c02a52ba4d11c2bef239e40
Padraig Gleeson <p.gleeson@gmail.com>
2020-03-09 16:50:38 +0000

    Regenerated & testing Brian examples

--- regenerate.sh
+++ regenerate.sh
@@ -7,3 +7,10 @@ cd SBML
 jnml -sbml-import BIOMD0000000184.xml 1000 0.01
 mv BIOMD0000000184_LEMS.xml ../LEMS
 
+cd ../LEMS
+
+jnml BIOMD0000000184_LEMS.xml -brian
+mv BIOMD0000000184_LEMS_brian.py ../Brian
+jnml BIOMD0000000184_LEMS.xml -brian2
+mv BIOMD0000000184_LEMS_brian2.py ../Brian
+
