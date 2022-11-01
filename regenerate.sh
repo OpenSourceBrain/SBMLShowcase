8b04d4f2bd7a9994af617bb0f351c82c63e0cfd6
Padraig Gleeson <p.gleeson@gmail.com>
2022-11-01 09:57:28 +0000

    Comments in regenerate.sh

--- regenerate.sh
+++ regenerate.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 set -ex
 
-# A script to test various combinations of import/export of SBML to/from NeuroML2 & LEMS
+### A script to test various combinations of import/export of SBML to/from NeuroML2 & LEMS
 
+## Test converting SBML to LEMS
 cd SBML
-
 pynml -sbml-import BIOMD0000000184.xml 1000 0.01
 pynml -sbml-import BIOMD0000000127_url.xml  140 0.01
 mv BIOMD00000*_LEMS.xml ../LEMS
 
-cd ../LEMS
 
+## Test executing generated LEMS with pyNeuroML
+cd ../LEMS
+pynml BIOMD0000000184_LEMS.xml -nogui
+
+
+## Test converting generated LEMS to Brian simulator code
 pynml BIOMD0000000184_LEMS.xml -brian
 mv BIOMD0000000184_LEMS_brian.py ../Brian
 pynml BIOMD0000000184_LEMS.xml -brian2
 mv BIOMD0000000184_LEMS_brian2.py ../Brian
 
 
-pynml BIOMD0000000184_LEMS.xml -neuron
+## Test converting generated LEMS to NEURON simulator code
+pynml BIOMD0000000184_LEMS.xml -neuron -run -nogui
 mv BIOMD0000000184_LEMS_nrn.py Lavrentovich2008_Ca_Oscillations_0.mod ../NEURON
 
-cd ../NeuroML2
 
+## Test converting a NeuroML+LEMS model to SBML+SED-ML
+cd ../NeuroML2
 jnml LEMS_NML2_Ex9_FN.xml -sbml-sedml
 mv *sedml ../SBML
 mv *sbml ../SBML
