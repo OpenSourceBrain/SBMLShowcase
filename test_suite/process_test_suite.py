b1854de37219590c86a433b546c1e4a6562c0cdd
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2023-12-11 12:54:53 +0000

    added test results summaries at the top of the markdown table and URL to test case files online

--- test_suite/process_test_suite.py
+++ test_suite/process_test_suite.py
@@ -14,7 +14,6 @@ import glob
 from pyneuroml.sbml import validate_sbml_files
 from pyneuroml.sedml import validate_sedml_files
 
-
 def parse_arguments():
     "Parse command line arguments"
 
@@ -41,6 +40,14 @@ def parse_arguments():
     )
 
     parser.add_argument(
+        "--suite-url-base",
+        action="store",
+        type=str,
+        default="https://github.com/sbmlteam/sbml-test-suite/blob/release/cases/semantic",
+        help="Base of the URL-to-suite-test-cases link to embed in results, use '' empty string to disable links",
+    )
+
+    parser.add_argument(
         "--output-file",
         action="store",
         type=str,
@@ -50,34 +57,75 @@ def parse_arguments():
 
     return parser.parse_args()
 
+#strings to use to represent passed and failed tests
+okay = "pass"
+fail = "FAIL"
 def pass_or_fail(result):
     '''
     convert True into "pass" and False into "fail"
     as otherwise it's not obvious in the table what True and False mean
     '''
-    return "pass" if result else "FAIL"
+    global okay,fail
 
+    return okay if result else fail
+
+
+def add_case_url(case,fpath,url_base):
+    '''
+    insert URL link to original test case file online
+    effectively replaces args.suite_path with args.suite_url_base
+    this should produce a valid link for all the main intended use case
+    of testing the sbml test suite using the default args
+    but will not handle all possible variations of globs and base directories
+    in which case it should be disabled by setting --suite-url-base=''
+    '''
+
+    url = os.path.join(url_base,fpath)
+    new_item = f'[{case}]({url})'
+    return new_item
 
 def process_cases(args):
     "process the test cases and write results out as a markdown table"
 
-    header = "|case|valid-sbml|valid-sbml-units|valid-sedml|\n"
-    sep = "|---|---|---|---|\n"
-    row = "|{case}|{valid_sbml}|{valid_sbml_units}|{valid_sedml}|\n"
+    header = "|case|valid-sbml|valid-sbml-units|valid-sedml|"
+    sep = "|---|---|---|---|"
+    row = "|{case}|{valid_sbml}|{valid_sbml_units}|{valid_sedml}|"
+    summary="|cases={n_cases}|fails={n_failing[valid_sbml]}|fails={n_failing[valid_sbml_units]}|fails={n_failing[valid_sedml]}|"
 
     with open(args.output_file, "w") as fout:
+        output = []
+        n_cases = 0
+        n_failing = {"valid_sbml":0, "valid_sbml_units":0, "valid_sedml":0 }
+
         os.chdir(args.suite_path)
-        fout.write(header+sep)
+
+        #accumulate output in memory so we can put the nifty summary at the top
+        #instead of at the end of 1800 lines
+        output.append(header)
+        output.append(sep)
+        output.append("<results summary goes here>")
+
         for fpath in sorted(glob.glob(args.suite_glob)):
             sedml_path = fpath.replace(".xml", "-sedml.xml")
             print(fpath)
             assert os.path.isfile(fpath)
             assert os.path.isfile(sedml_path)
             case = os.path.basename(fpath)
+            if args.suite_url_base != '': case = add_case_url(case,fpath,args.suite_url_base)
             valid_sbml = pass_or_fail(validate_sbml_files([fpath], strict_units=False))
             valid_sbml_units = pass_or_fail(validate_sbml_files([fpath], strict_units=True))
             valid_sedml = pass_or_fail(validate_sedml_files([sedml_path]))
-            fout.write(row.format(**locals()))
+            output.append(row.format(**locals()))
+
+            #tally results so we provide a summary
+            global okay,fail
+            n_cases +=1
+            if valid_sbml != okay: n_failing["valid_sbml"] += 1
+            if valid_sbml_units != okay: n_failing["valid_sbml_units"] += 1
+            if valid_sedml != okay: n_failing["valid_sedml"] += 1
+            output[2] = summary.format(**locals())
+
+        for line in output: fout.write(line+'\n')
 
 
 if __name__ == "__main__":
