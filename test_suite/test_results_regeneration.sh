235d2ca5a1ffdf182571a2abfc6726f2ff79e37f
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-02-26 12:36:42 +0000

    match upstream generator script to avoid conflict

--- test_suite/test_results_regeneration.sh
+++ test_suite/test_results_regeneration.sh
@@ -6,11 +6,9 @@
 
 set -ex
 
-# Install wget and unzip if missing locally, eg for ubuntu/debian systems:
-#   sudo apt-get install wget unzip --fix-missing --yes
+# Install if missing locally
+# sudo apt-get install wget unzip --fix-missing --yes
 
-# Uncomment the wget and unzip commands if you don't already have the zip file downloaded
 wget https://github.com/sbmlteam/sbml-test-suite/releases/download/3.4.0/semantic_tests_with_sedml_and_graphs.v3.4.0.zip
 unzip semantic_tests_with_sedml_and_graphs.v3.4.0.zip
-
 ./process_test_suite.py --suite-path ./semantic --suite-glob '*/*-sbml-l3v2.xml' --output-file ./results.md
