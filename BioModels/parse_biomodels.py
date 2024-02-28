c1799646ea424a4a038009ec14c1af03adf90ed3
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-02-28 13:00:47 +0000

    refactored initial generator script slightly

--- BioModels/parse_biomodels.py
+++ BioModels/parse_biomodels.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import requests
 
@@ -7,7 +8,7 @@ out_format="json"
 
 def get_model_identifiers():
 
-    response = requests.get(API_URL + "/model/identifiers?format=" + out_format)
+    response = requests.get(f"{API_URL}/model/identifiers?format={out_format}")
     response.raise_for_status()
     output = response.json()
 
@@ -16,16 +17,14 @@ def get_model_identifiers():
 
 def get_model_info(model_id):
 
-    response = requests.get(API_URL + "/"+model_id+"?format=" + out_format)
+    response = requests.get(f"{API_URL}/{model_id}?format={out_format}")
     response.raise_for_status()
     output = response.json()
 
     return output
 
 
-
-if __name__ == "__main__":
-
+def main():
     model_ids = get_model_identifiers()['models'] 
 
     max_count = 3000
@@ -37,33 +36,43 @@ if __name__ == "__main__":
     sep = "|---|---|---|---|"
     row = "|{model_link}<br/><sup>{model_name}</sup>|???|???|{sedml_link}|"
 
+    output = []
+    output.append(header)
+    output.append(sep)
+
+    for model_id in model_ids:
+        #allow testing of small samples of models
+        if count >= max_count: break
+
+        #BIOMD ids should be the curated ones
+        if not 'BIOMD' in model_id: continue
+
+        model_link = f'[{model_id}]({API_URL}/{model_id})' #used via locals()
+        info = get_model_info(model_id)
+        model_name = info['name']                          #used via locals()
+        print("=======================")
+        print(f"  {model_id}: {info['name']}")
+        #print(info.keys())
+        for file in info['files']['main']:
+            print('   %s'%file)
+
+        if not 'additional' in info['files']: continue
+
+        for file in info['files']['additional']:
+            if 'SED-ML' in file['name'] or 'SED-ML' in file['description']:
+                with_sedml.append(model_id)
+                print('   - SED-ML: %s'%file)
+
+                sedml_link = f'[{file["name"]}]({API_URL}/{model_id}#Files)' #used via locals()
+                output.append(row.format(**locals()))
+
+        count +=1 
+
+    #write out to file
     with open('README.md', "w") as fout:
-        output = []
-        output.append(header)
-        output.append(sep)
-
-        for model_id in model_ids:
-            if count<max_count:
-                if 'BIOMD' in model_id:
-                    model_link = f'[{model_id}](https://www.ebi.ac.uk/biomodels/{model_id})'
-                    info = get_model_info(model_id)
-                    model_name = info['name']
-                    print("=======================")
-                    print(f"  {model_id}: {info['name']}")
-                    #print(info.keys())
-                    for file in info['files']['main']:
-                        print('   %s'%file)
-                    if 'additional' in info['files']:
-                        for file in info['files']['additional']:
-                            if 'SED-ML' in file['name'] or 'SED-ML' in file['description']:
-                                with_sedml.append(model_id)
-                                print('   - SED-ML: %s'%file)
-
-                                sedml_link = f'[{file["name"]}](https://www.ebi.ac.uk/biomodels/{model_id}#Files)'
-                                output.append(row.format(**locals()))
-
-                    count +=1 
-        
         for line in output: fout.write(line+'\n')
 
     print("Found %i/%i containing SED-ML: %s"%(len(with_sedml), count, with_sedml))
+
+if __name__ == "__main__":
+    main()
