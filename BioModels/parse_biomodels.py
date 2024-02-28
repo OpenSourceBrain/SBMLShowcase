fde7c3599e50010448e9ef22e1f6d75311c06b89
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-02-28 16:25:00 +0000

    continuing work on BioModel table generator script

--- BioModels/parse_biomodels.py
+++ BioModels/parse_biomodels.py
@@ -7,12 +7,15 @@ API_URL: str = "https://www.ebi.ac.uk/biomodels"
 out_format="json"
 
 def get_model_identifiers():
+    '''
+    get list of all model ids
+    '''
 
     response = requests.get(f"{API_URL}/model/identifiers?format={out_format}")
     response.raise_for_status()
     output = response.json()
 
-    return output
+    return output['models']
 
 
 def get_model_info(model_id):
@@ -25,54 +28,69 @@ def get_model_info(model_id):
 
 
 def main():
-    model_ids = get_model_identifiers()['models'] 
+    #get list of all available models
+    model_ids = get_model_identifiers() 
 
-    max_count = 3000
+    max_count = 0 #0 for unlimited
     count = 0
 
-    with_sedml = []
+    #models_with_sedml = []
 
-    header = "|Model|valid-sbml|valid-sbml-units|SED-ML|"
-    sep = "|---|---|---|---|"
-    row = "|{model_link}<br/><sup>{model_name}</sup>|???|???|{sedml_link}|"
+    header = "|Model|SBML|SEDML|valid-sbml|valid-sbml-units|valid-sedml|tellurium|"
+    sep = "|---|---|---|---|---|---|---|"
+    row = "|{model_link}<br/><sup>{model_name}</sup>|{sbml_file}|{sedml_file}| | | |"
 
     output = []
     output.append(header)
     output.append(sep)
 
     for model_id in model_ids:
-        #allow testing of small samples of models
-        if count >= max_count: break
+        #allow testing on a small sample of models
+        if max_count > 0 and count >= max_count: break
+        count +=1
 
-        #BIOMD ids should be the curated ones
+        #BIOMD ids should be the curated models
         if not 'BIOMD' in model_id: continue
 
-        model_link = f'[{model_id}]({API_URL}/{model_id})' #used via locals()
         info = get_model_info(model_id)
-        model_name = info['name']                          #used via locals()
-        print("=======================")
-        print(f"  {model_id}: {info['name']}")
-        #print(info.keys())
-        for file in info['files']['main']:
-            print('   %s'%file)
 
+        #handle only single SBML files (some are Open Neural Network Exchange, or "Other" such as Docker)
+        if not info['format']['name'] == "SBML": continue
+        if not len(info['files']['main']) == 1: continue
+
+        #must have a SEDML file as well in order to be executed
         if not 'additional' in info['files']: continue
 
-        for file in info['files']['additional']:
-            if 'SED-ML' in file['name'] or 'SED-ML' in file['description']:
-                with_sedml.append(model_id)
-                print('   - SED-ML: %s'%file)
+        model_link = f'[{model_id}]({API_URL}/{model_id})' #used via locals()
+        model_name = info['name']                          #used via locals()
+        sbml_file = info['files']['main'][0]
 
-                sedml_link = f'[{file["name"]}]({API_URL}/{model_id}#Files)' #used via locals()
-                output.append(row.format(**locals()))
+        sedml_file = []
+        for file_info in info['files']['additional']:
+            if 'SED-ML' in file_info['name'] or 'SED-ML' in file_info['description']:
+                sedml_file.append(file_info['name'])
 
-        count +=1 
+        #require exactly one SEDML file
+        if len(sedml_file) != 1: continue
+        sedml_file = sedml_file[0]
+
+        #models_with_sedml.append(model_id)
+
+        #sedml_link = f'[{file["name"]}]({API_URL}/{model_id}#Files)' #used via locals()
+
+        output.append(row.format(**locals()))
+
+        # print("=======================")
+        # print(f"{model_id}: {info['name']}")
+        # print(f"   SBML file:  {sbml_file}")
+        # print(f"   SEDML file: {sedml_file}")
+
 
     #write out to file
     with open('README.md', "w") as fout:
         for line in output: fout.write(line+'\n')
 
-    print("Found %i/%i containing SED-ML: %s"%(len(with_sedml), count, with_sedml))
+    #print("Found %i/%i containing SED-ML: %s"%(len(models_with_sedml), count, models_with_sedml))
 
 if __name__ == "__main__":
     main()
