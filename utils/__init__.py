9e26ea3d833ecd57b6decc45b528b8be7560c1da
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-04-24 10:57:06 +0000

    trying to capture log.yml error message

--- utils/__init__.py
+++ utils/__init__.py
@@ -72,6 +72,9 @@ def create_omex(sedml_file,sbml_file):
 
     return data_dir,omex_file
 
+def read_log_yml(data_dir):
+    return None
+
 def run_biosimulators_docker(engine,sedml_file,sbml_file,error_categories=error_categories):
     '''
     try to run the sedml+sbml combo using biosimulators
@@ -97,8 +100,14 @@ def run_biosimulators_docker(engine,sedml_file,sbml_file,error_categories=error_
                             command=f"-i /root/in/{omex_file} -o /root/out")
         return "pass" #no errors
     except Exception as e:
-        #make an error string which doesn't break markdown tabless
+        #capture the error as a string which won't break markdown tables
         error_str = safe_md_string(e)
+
+    #try to load the cleaner error message from the log.yml file
+    log_str = read_log_yml(data_dir)
+
+    if log_str:
+        error_str = safe_md_string(log_str)
 
     #categorise the error string
     for tag in error_categories[engine]:
