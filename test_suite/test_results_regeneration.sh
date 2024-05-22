a5f9824ff9cac40bb7756ccbc75c06b5c21b161e
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2024-05-22 10:40:44 +0000

    suppressing wget and unzip verbose output to make CI runner output readable on GH

--- test_suite/test_results_regeneration.sh
+++ test_suite/test_results_regeneration.sh
@@ -11,11 +11,11 @@ set -ex
 
 #download and extract test suite files in not already present
 if [[ ! -f semantic_tests_with_sedml_and_graphs.v3.4.0.zip ]] ; then
-    wget https://github.com/sbmlteam/sbml-test-suite/releases/download/3.4.0/semantic_tests_with_sedml_and_graphs.v3.4.0.zip
+    wget --quiet https://github.com/sbmlteam/sbml-test-suite/releases/download/3.4.0/semantic_tests_with_sedml_and_graphs.v3.4.0.zip
 fi
 
 if [[ ! -d semantic ]] ; then
-    unzip semantic_tests_with_sedml_and_graphs.v3.4.0.zip
+    unzip -qq semantic_tests_with_sedml_and_graphs.v3.4.0.zip
 fi
 
 #run the tests, output markdown table with summary
