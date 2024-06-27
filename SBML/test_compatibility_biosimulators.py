fb365739a80e57ab717faee94745864a3656c84a
Stella <30465823+stellaprins@users.noreply.github.com>
2024-06-27 22:27:48 +0100

    Add draft for test_compatibility_biosimulators.py which creates preliminary results table and update jupyter notebook

--- SBML/test_compatibility_biosimulators.py
+++ SBML/test_compatibility_biosimulators.py
@@ -1,76 +1,161 @@
-#!/usr/bin/env python
+'''
+This script tests the compatibility of different biosimulation engines with a given SBML and SED-ML file.
+It runs each engine and records the result (pass/fail) and any error messages encountered during the simulation.
+The results are then displayed in a table and saved to a markdown file.
+'''
 
 import sys
 sys.path.append("..")
 import utils
 import os
 import pandas as pd
-import shutil
 
 sbml_filepath = 'LEMS_NML2_Ex9_FN.sbml'
-sedml_filepath = 'LEMS_NML2_Ex9_FN_missing_xmlns.sedml' #xmlns:sbml missing
+sedml_filepath = 'LEMS_NML2_Ex9_FN_missing_xmlns.sedml' #xmlns:sbml missing (original file)
 
-# Instruction are in different format for these three engines
-# OpenCOR:https://docs.biosimulators.org/Biosimulators_OpenCOR/installation.html#python-package-and-command-line-program
-# rbapy: https://docs.biosimulators.org/Biosimulators_RBApy/installation.html#command-line-program
-# smoldyn: https://smoldyn.readthedocs.io/en/latest/python/api.html#sed-ml-combine-biosimulators-api
 
-engines = ['amici',\
-           'brian2',\
-           'bionetgen',\
-           'boolnet',\
-           'cbmpy',\
-           'cobrapy',\
-           'copasi',\
-           'gillespy2',\
-           'copasi',\
-           'tellurium',\
-           'libsbmlsim',\
-           'ginsim',\
-           'masspy',\
-           'opencor',\
-           'pysces',\
-           'rbapy',\
-           'vcell',\
-           'xpp',  
-           'smoldyn'  
+engines = utils.engines
+types_dict = utils.types_dict
 
-           ]
-
-# to store results
 engine_dict = {}
 
-for e in engines:
+for e in engines.keys():
     print('Running ' + e)
     output_dir = os.path.abspath(os.path.join('output', e))
     try:
         record = utils.run_biosimulators_docker(e, sedml_filepath, sbml_filepath, output_dir=output_dir)
         engine_dict[e] = record
     except Exception as error:
-        # get error message
         error_message = str(error)
         print(f"Error occurred while running {e}")
         engine_dict[e] = error_message
         continue
 
-results_table = pd.DataFrame.from_dict(engine_dict, orient='index', columns=['Result'])
+utils.delete_output_folder('output')
+
+# Create a table of the results
+results_table = pd.DataFrame.from_dict(engine_dict).T
+results_table.columns = ['pass/FAIL', 'Error']
 results_table.index.name = 'Engine'
 results_table.reset_index(inplace=True)
-results_table.sort_values('Engine', inplace=True)
-results_table['pass/Fail'] = results_table['Result'].apply(lambda x: x[0] if isinstance(x, list) else x)
-results_table['Error'] = results_table['Result'].apply(lambda x: x[1] if isinstance(x, list) else None)
-results_table['Error'] = results_table['Error'].apply(lambda x: x.replace('```', '') if x else x)
-results_table.drop(columns=['Result'], inplace=True)
-results_table.to_markdown('results.md', index=False)
 
-# Save the results table to a markdown file
-with open('results.md', 'w') as f:
-    f.write(results_table.to_markdown(index=False))
+results_table['Error'] = results_table.apply(lambda x: None if x['pass/FAIL'] == x['Error'] else x['Error'], axis=1)
+results_table['pass/FAIL'] = results_table['pass/FAIL'].replace('other', 'FAIL')
 
-# Delete the output folder and its contents
-for file_name in os.listdir('output'):
-    file_path = os.path.join('output', file_name)
-    if os.path.isfile(file_path):
-        os.remove(file_path)
-    elif os.path.isdir(file_path):
-        shutil.rmtree(file_path)
+results_table['Error'] = results_table['Error'].apply(lambda x: utils.parse_error_message(x))
+results_table['Error'] = results_table['Error'].apply(lambda x: utils.collapsible_content(x))
+
+results_table['Compatibility'] = results_table['Engine'].apply(lambda x: utils.check_file_compatibility_test(x, types_dict, sbml_filepath, sedml_filepath))
+results_table['Compatibility'] = results_table['Compatibility'].apply(lambda x: utils.collapsible_content(x[1], title=x[0]))
+results_table['pass/FAIL'] = results_table['pass/FAIL'].apply(lambda x: f'<span style="color:darkred;">{x}</span>' if x == 'FAIL' else x)
+results_table['Compatibility'] = results_table['Compatibility'].apply(lambda x: f'<span style="color:darkred;">{x}</span>' if 'FAIL' in x else x)
+
+results_table = results_table.to_markdown(index=False)
+
+# save results_md_table
+with open('results_biosimulators_compatability.md', 'w', encoding='utf-8') as f:
+    f.write(results_table)
+
+# #!/usr/bin/env python
+
+# import sys
+# sys.path.append("..")
+# import utils
+# import os
+# import pandas as pd
+# import shutil
+
+# sbml_filepath = 'LEMS_NML2_Ex9_FN.sbml'
+# sedml_filepath = 'LEMS_NML2_Ex9_FN_missing_xmlns.sedml' #xmlns:sbml missing
+
+# # Instruction are in different format for these three engines
+# # OpenCOR:https://docs.biosimulators.org/Biosimulators_OpenCOR/installation.html#python-package-and-command-line-program
+# # rbapy: https://docs.biosimulators.org/Biosimulators_RBApy/installation.html#command-line-program
+# # smoldyn: https://smoldyn.readthedocs.io/en/latest/python/api.html#sed-ml-combine-biosimulators-api
+
+# engines = ['amici',\
+#            'brian2',\
+#            'bionetgen',\
+#            'boolnet',\
+#            'cbmpy',\
+#            'cobrapy',\
+#            'copasi',\
+#            'gillespy2',\
+#            'copasi',\
+#            'tellurium',\
+#            'libsbmlsim',\
+#            'ginsim',\
+#            'masspy',\
+#            'netpyne',\
+#            'neuron',\
+#            'opencor',\
+#            'pyneuroml',\
+#            'pysces',\
+#            'rbapy',\
+#            'vcell',\
+#            'xpp',  
+#            'smoldyn'  
+#            ]
+
+# engines = {
+#                         'amici': ('sbml', 'sedml'),\
+#                         'brian2': [('nml', 'sedml'),('lems', 'sedml')],\
+#                         'bionetgen': ('bngl', 'sedml'),\
+#                         'boolnet': ('sbmlqual', 'sedml'),\
+#                         'cbmpy': ('sbml', 'sedml'),\
+#                         'cobrapy': ('sbml', 'sedml'),\
+#                         'copasi': ('sbml', 'sedml'),\
+#                         'gillespy2': ('sbml', 'sedml'),\
+#                         'ginsim': ('sbmlqual', 'sedml'),\
+#                         'libsbmlsim': ('sbml', 'sedml'),\
+#                         'masspy': ('sbml', 'sedml'),\
+#                         'netpyne': ('sbml', 'sedml'),\
+#                         'neuron': [('nml', 'sedml'),('lems', 'sedml')],\
+#                         'opencor': ('sbml', 'sedml'),\
+#                         'pyneuroml': [('nml', 'sedml'),('lems', 'sedml')],\
+#                         'pysces': ('sbml', 'sedml'),\
+#                         'rbapy': ('rbapy', 'sedml'),\
+#                         'smoldyn':None ,\
+#                         'tellurium': ('sbml', 'sedml'),\
+#                         'vcell': None,\
+#                         'xpp': ('xpp', 'sedml')               
+#                         }
+
+
+# # to store results
+# engine_dict = {}
+
+# for e in engines.keys():
+#     print('Running ' + e)
+#     output_dir = os.path.abspath(os.path.join('output', e))
+#     try:
+#         record = utils.run_biosimulators_docker(e, sedml_filepath, sbml_filepath, output_dir=output_dir)
+#         engine_dict[e] = record
+#     except Exception as error:
+#         # get error message
+#         error_message = str(error)
+#         print(f"Error occurred while running {e}")
+#         engine_dict[e] = error_message
+#         continue
+
+# results_table = pd.DataFrame.from_dict(engine_dict, orient='index', columns=['Result'])
+# results_table.index.name = 'Engine'
+# results_table.reset_index(inplace=True)
+# results_table.sort_values('Engine', inplace=True)
+# results_table['pass/Fail'] = results_table['Result'].apply(lambda x: x[0] if isinstance(x, list) else x)
+# results_table['Error'] = results_table['Result'].apply(lambda x: x[1] if isinstance(x, list) else None)
+# results_table['Error'] = results_table['Error'].apply(lambda x: x.replace('```', '') if x else x)
+# results_table.drop(columns=['Result'], inplace=True)
+# results_table.to_markdown('results.md', index=False)
+
+# # Save the results table to a markdown file
+# with open('results.md', 'w') as f:
+#     f.write(results_table.to_markdown(index=False))
+
+# # Delete the output folder and its contents
+# for file_name in os.listdir('output'):
+#     file_path = os.path.join('output', file_name)
+#     if os.path.isfile(file_path):
+#         os.remove(file_path)
+#     elif os.path.isdir(file_path):
+#         shutil.rmtree(file_path)
--- utils/__init__.py
+++ utils/__init__.py
@@ -18,33 +18,42 @@ import libsbml
 import libsedml
 import tempfile
 
-#define error categories for detailed error counting per engine
-# (currently only tellurium)
-# key is the tag/category used to report the category, value is a regex matching the error message
-# see MarkdownTable.process_engine_outcomes
+# 
+engines = {
+                        'amici': ('sbml', 'sedml'),\
+                        'brian2': [('nml', 'sedml'),('lems', 'sedml'),('sbml', 'sedml')],\
+                        'bionetgen': ('bngl', 'sedml'),\
+                        'boolnet': ('sbmlqual', 'sedml'),\
+                        'cbmpy': ('sbml', 'sedml'),\
+                        'cobrapy': ('sbml', 'sedml'),\
+                        'copasi': ('sbml', 'sedml'),\
+                        'gillespy2': ('sbml', 'sedml'),\
+                        'ginsim': ('sbmlqual', 'sedml'),\
+                        'libsbmlsim': ('sbml', 'sedml'),\
+                        'masspy': ('sbml', 'sedml'),\
+                        'netpyne': ('sbml', 'sedml'),\
+                        'neuron': [('nml', 'sedml'),('lems', 'sedml')],\
+                        'opencor': ('sbml', 'sedml'),\
+                        'pyneuroml': [('nml', 'sedml'),('lems', 'sedml')],\
+                        'pysces': ('sbml', 'sedml'),\
+                        'rbapy': ('rbapy', 'sedml'),\
+                        'smoldyn':None ,\
+                        'tellurium': ('sbml', 'sedml'),\
+                        'vcell': None,\
+                        'xpp': ('xpp', 'sedml')               
+            }
 
-
-# error_categories=\
-# {
-#     "tellurium":
-#         {
-#             "algebraic":"^Unable to support algebraic rules.",
-#             "delay":"^Unable to support delay differential equations.",
-#             "ASTNode":"^Unknown ASTNode type of",
-#             "stochiometry":"^Mutable stochiometry for species which appear multiple times in a single reaction",
-#             "float":"^'float' object is not callable",
-#             "SpeciesRef":"is not a named SpeciesReference",
-#             "reset":"reset",
-#             "SEDMLfile":"^failed to validate SEDML file",
-#             "NoSBMLelement":"^No sbml element exists",
-#             "CV_ERR_FAILURE":"CV_ERR_FAILURE",
-#             "CV_TOO_MUCH_WORK":"CV_TOO_MUCH_WORK",
-#             "CV_CONV_FAILURE":"CV_CONV_FAILURE",
-#             "CV_ILL_INPUT":"CV_ILL_INPUT",
-#             "OutOfRange":"list index out of range",
-#         },
-#     "copasi":{},
-# }
+types_dict = {
+                'sbml':'SBML',\
+                'sedml':'SED-ML',\
+                'nml':'NeuroML',\
+                'lems':'LEMS',\
+                'sbmlqual':'SBML-qual',\
+                'bngl':'BNGL',\
+                'rbapy':'RBApy',\
+                'xpp':'XPP',\
+                'smoldyn':'Smoldyn'\
+             }
 
 error_categories = {
             "algebraic":"^Unable to support algebraic rules.",
@@ -217,6 +226,108 @@ def read_log_yml(log_filepath):
         ym = yaml.safe_load(f)
     return ym['exception']['message']
 
+def parse_error_message(text):
+    if text != None:
+        text_message = re.findall(r'"([^"]*)"', text) 
+        if len(text_message) > 0:
+            text = text_message
+        else:
+            text = text.replace('|', '')
+            return text
+        text = bytes(text[0], "utf-8").decode("unicode_escape")
+        text = text.replace('|', '')
+
+        # # for any text with "<*>" remove "<" as well as ">" but leave wildcard text *
+        text = re.sub(r'<([^>]*)>', r'\1', text)
+
+        # replace color codes with html color codes
+        text = text.replace("\x1b[33m",'<span style="color:darkorange;">')
+        text = text.replace("\x1b[31m",'<span style="color:red;">')
+
+        # # remove .\x1b[0m
+        text = text.replace("\x1b[0m", "")
+
+        # find first "." or ":" after "<span*" and add "</span>"after it
+        pattern = r'(<span style="[^"]*">[^.:]*)([.:])'
+        replacement = r'\1\2</span>'
+        text = re.sub(pattern, replacement, text, count=1)
+
+        # bullet points and new lines
+        text = text.replace('\r\n  - ', '</li><li>')
+        text = text.replace('\r\n', '<br>')
+        text = text.replace('\n', '<br>') 
+
+        # BioSimulatorsWarning:  two <br> tags after
+        text = text.replace('BioSimulatorsWarning:', '<br><br>BioSimulatorsWarning:<br><br>')
+        text = text.replace('warnings.warn(termcolor.colored(message, Colors.warning.value), category)', '<br>')
+
+        # if text includes The COMBINE/OMEX did not execute successfully: make everyhting from that point red
+        text = text.replace('The COMBINE/OMEX did not execute successfully:', '<span style="color:red;">The COMBINE/OMEX did not execute successfully:')
+    return text
+
+def display_error_message(error_message):
+    if error_message != None:
+        display_markdown(f'{error_message}', raw=True)
+    return error_message
+
+def check_file_compatibility_test(engine, types_dict, model_filepath, experiment_filepath):
+    '''
+    Check if the file extensions suggest the file types are compatible with the engine
+    '''
+    input_filetypes = set(get_filetypes(model_filepath, experiment_filepath))
+    input_file_types_text = [types_dict[i] for i in input_filetypes]
+
+
+    engine_filetypes = engines[engine]
+    if engine_filetypes is not None:
+        # Flatten the list if the engine_filetypes is a list of tuples
+        if all(isinstance(i, tuple) for i in engine_filetypes):
+            engine_filetypes = {item for sublist in engine_filetypes for item in sublist}
+        engine_file_types_text = [types_dict[i] for i in engine_filetypes if i in types_dict]
+        if input_filetypes.issubset(engine_filetypes):
+            return 'pass', (f"The file extensions suggest the input file types are '{input_file_types_text}'. These are compatible with {engine}")
+        else:
+            return 'FAIL', (f"The file extensions suggest the input file types are '{input_file_types_text}'. Tese are not compatible with {engine}. The following file types will be compatible {engine_file_types_text}")
+    else:
+        return 'FAIL', (f"{engine} compatible file types unknown.")
+
+
+def collapsible_content(content, title='Details'):
+    """
+    Create a collapsible content section in markdown format
+    
+    Input: content, title
+    """
+    if content:
+        return f'<details><summary>{title}</summary>{content}</details>'
+    else:
+        return None
+    
+def get_filetypes(model_filepath, simulation_filepath):
+    """
+    Get the filetypes of the model and simulation files
+
+    Input: model_filepath, simulation_filepath
+    Output: tuple of filetypes
+    """
+    if model_filepath.endswith(".sbml") and simulation_filepath.endswith(".sedml"):
+        filetypes = ('sbml', 'sedml')
+    else:
+        filetypes = "other"
+    return filetypes
+
+def delete_output_folder(output_dir):
+    '''
+    # Delete the output folder and its contents
+    '''
+    for file_name in os.listdir(output_dir):
+        file_path = os.path.join(output_dir, file_name)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+
+
 def run_biosimulators_docker(engine,sedml_filepath,sbml_filepath,output_dir=None,error_categories=error_categories):
     '''
     put the sedml and sbml file into an omex archive
@@ -232,13 +343,14 @@ def run_biosimulators_docker(engine,sedml_filepath,sbml_filepath,output_dir=None
         return "pass" #no errors
     except Exception as e:
         #capture the error as a string which won't break markdown tables
-        error_str = safe_md_string(e)
+        # error_str = safe_md_string(e)
+        error_str = str(e)
 
-    #try to load the cleaner error message from the log.yml file
-    log_str = read_log_yml(os.path.join(os.path.dirname(omex_filepath),"log.yml"))
+    # #try to load the cleaner error message from the log.yml file
+    # log_str = read_log_yml(os.path.join(os.path.dirname(omex_filepath),"log.yml"))
 
-    if log_str:
-        error_str = safe_md_string(log_str)
+    # if log_str:
+    #     error_str = safe_md_string(log_str)
 
     # #categorise the error string
     # for tag in error_categories:
