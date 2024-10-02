9e6697d58917dab9e46042e4c0ba9a81be1ad8a0
Stella <30465823+stellaprins@users.noreply.github.com>
2024-10-02 15:51:47 +0100

    Add a test to determine whether the biosimulators API is working remotely

---
+++ SBML/tests/test_biosimulators_api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Test health status of the BioSimulators API."""
+
+import requests 
+
+biosimulations_api_url = "https://api.biosimulations.org/health"
+
+r = requests.get(biosimulations_api_url)  
+r_status = r.json()["status"]
+
+if r_status == "ok":
+    exit_status = 0
+else:
+    exit_status = 1
