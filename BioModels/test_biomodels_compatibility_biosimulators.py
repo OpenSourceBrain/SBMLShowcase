edcd9e14b924315307d5fa478c64e579d6832b21
Stella <30465823+stellaprins@users.noreply.github.com>
2024-11-21 10:27:59 +0000

    add BIOMD0000000724

--- BioModels/test_biomodels_compatibility_biosimulators.py
+++ BioModels/test_biomodels_compatibility_biosimulators.py
@@ -163,15 +163,9 @@ def main():
 
 
 if __name__ == "__main__":
-
-
     use_original_files = False
-    # engine_list = None
-
-    biomodel_dict = {"BIOMD0000000001": {"outputStartTime": "0", "outputEndTime": "10000", "numberOfSteps": "100000"},
-                     "BIOMD0000000138": {"outputStartTime": "0", "outputEndTime": "10", "numberOfSteps": "1000"}}
-    
-    biomodel_id_list = list(biomodel_dict.keys())
-    biomodel_id_list = ["BIOMD0000001077"]
-
+    biomodel_id_list = ["BIOMD0000000001",
+                        "BIOMD0000000138",
+                        "BIOMD0000000724",
+                        "BIOMD0000001077"]
     main()
