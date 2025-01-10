9849f3f331df1edfc6d96bff1e3197fd81b49a77
Stella <30465823+stellaprins@users.noreply.github.com>
2025-01-10 14:34:28 +0000

    fix display test suite

--- test_suite/process_test_suite.py
+++ test_suite/process_test_suite.py
@@ -484,22 +484,18 @@ def process_cases(args):
         if subfolder in remote_results.keys():
             for e in list(remote_results[subfolder].keys()):
                 print(f"Processing remote results for {subfolder} with engine {e}")
-                result = remote_results[subfolder][e]
-                link = remote_links[subfolder][e]
 
-                error_message_string = ""
-                if result["error_message"]:
-                    error_message = utils.safe_md_string(result["error_message"])
-                    exception_type = utils.safe_md_string(result["exception_type"])
-                    error_message_string = f"<br><br>Error message: ```{error_message}```<br>Exception type: ```{exception_type}```"
-
-                info_submission = (
-                    f'<details><summary>{result["status"]}</summary>'
-                    f'[Download]({link["download"]})<br>'
-                    f'[Logs]({link["logs"]})<br>'
-                    f'[View]({link["view"]})<br>'
-                    f'HTTP response: {link["response"]}{error_message_string}'
-                )
+                if remote_results[subfolder][e]["error_message"] != "":
+                    error_message = utils.safe_md_string(
+                        remote_results[subfolder][e]["error_message"]
+                    )
+                    exception_type = utils.safe_md_string(
+                        remote_results[subfolder][e]["exception_type"]
+                    )
+                    error_message_string = f"Error message: ```{error_message}```<br><br>Exception type: ```{exception_type}```"
+                    info_submission = f'<details><summary>{remote_results[subfolder][e]["status"]}</summary>[Download]({remote_links[subfolder][e]["download"]})<br>[Logs]({remote_links[subfolder][e]["logs"]})<br>[View]({remote_links[subfolder][e]["view"]})<br><br>HTTP response: {str(remote_links[subfolder][e]["response"])}<br><br>{error_message_string}'
+                else:
+                    info_submission = f'<details><summary>{remote_results[subfolder][e]["status"]}</summary>[Download]({remote_links[subfolder][e]["download"]})<br>[Logs]({remote_links[subfolder][e]["logs"]})<br>[View]({remote_links[subfolder][e]["view"]})<br><br>HTTP response: {str(remote_links[subfolder][e]["response"])}'
 
                 mtab_remote_outcome_key = f"{e}_remote_outcome"
                 mtab[mtab_remote_outcome_key] = info_submission
